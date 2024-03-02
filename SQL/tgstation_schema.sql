23664ed1ac1f598d2bed8516201a2740ba71e326
daxsc <32306994+daxsc@users.noreply.github.com>
2024-03-03 00:06:03 +0100

    add known alts to db schema

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -119,6 +119,19 @@ CREATE TABLE `connection_log` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `known_alts`
+--
+DROP TABLE IF EXISTS `known_alts`;
+CREATE TABLE `known_alts` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `ckey1` VARCHAR(32) NOT NULL,
+    `ckey2` VARCHAR(32) NOT NULL,
+    `admin_ckey` VARCHAR(32) NOT NULL DEFAULT '*no key*',
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `unique_contraints` (`ckey1` , `ckey2`)
+);
+
+--
 -- Table structure for table `death`
 --
 
